e8960cb3bf5cadd8451c6798e499a31140fc5ffc
daniel <dmjames04@yahoo.com>
2018-07-06 21:38:16 +0100

    new files

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+title = document.getElementById("title");
+defence = document.getElementById("defence");
+attack = document.getElementById("attack");
+skills = document.getElementById("skills");
 
+
+skills.addEventListener("click", () => {
+defence.textContent = Math.floor(Math.random() * 26)
+attack.textContent = Math.floor(Math.random() * 26)
+
+});
 
 
 
