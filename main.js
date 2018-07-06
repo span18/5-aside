89f7ff882a66af980d0688efa61f7f2800075e6e
daniel <dmjames04@yahoo.com>
2018-07-06 22:52:46 +0100

    updating defence stats

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-title = document.getElementById("title");
-defence = document.getElementById("defence");
-attack = document.getElementById("attack");
+(d => {
+
+defence = document.getElementById("defence-one");
+attack = document.getElementById("attack-one");
 skills = document.getElementById("skills");
 
 
@@ -8,7 +9,24 @@ skills.addEventListener("click", () => {
 defence.textContent = Math.floor(Math.random() * 26)
 attack.textContent = Math.floor(Math.random() * 26)
 
-});
+})
+
+})(document);
+
+(d => {
+
+defenceTwo = document.getElementById("defence-two");
+attackTwo = document.getElementById("attack-two");
+skills = document.getElementById("skills");
+
+
+skills.addEventListener("click", () => {
+defenceTwo.textContent = Math.floor(Math.random() * 26)
+attackTwo.textContent = Math.floor(Math.random() * 26)
+
+})
+
+})(document);
 
 
 
