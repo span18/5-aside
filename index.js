9e9049e915608afe6748faeef54fa0782bea55b4
daniel <dmjames04@yahoo.com>
2018-07-06 22:12:09 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-title = document.getElementById("title");
-defence = document.getElementById("defence");
-attack = document.getElementById("attack");
-skills = document.getElementById("skills");
+(d => {
+    let todo = d.getElementById("text");
+    let add = d.getElementById("add");
+    let list = d.getElementById("list");
+
+    add.addEventListener("click", () => {
+        let li = d.createElement("li");
+        li.textContent = todo.value;
+
+        //li.classList.add("list-group-item");
+        todo.value = "";
+        list.insertBefore(li, list.firstElementChild);
+    })
+
+})(document);
+
+(d => {
+
+let list = d.getElementById("list");
+button = d.getElementById("shuffle");
+    function shuffleNodes() {
+        let nodes = list.children, i = 0;
+        nodes = Array.prototype.sort.call(nodes);
+        while(i < nodes.length) {
+           list.appendChild(nodes[i]);
+           ++i;
+        }
+    }
+    button.onclick = shuffleNodes;
+
+})(document);
 
 
-skills.addEventListener("click", () => {
-defence.textContent = Math.floor(Math.random() * 26)
-attack.textContent = Math.floor(Math.random() * 26)
-
-});
